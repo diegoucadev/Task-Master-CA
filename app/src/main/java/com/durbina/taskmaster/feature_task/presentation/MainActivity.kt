6d693a6d4422b68d919081263bbabec8f922d57d
Diego Urbina <00544424@uca.edu.sv>
2024-05-28 21:24:13 -0600

    chore: initial commit

---
+++ app/src/main/java/com/durbina/taskmaster/feature_task/presentation/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.durbina.taskmaster.feature_task.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.durbina.taskmaster.ui.theme.TaskMasterCleanArchitectureTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TaskMasterCleanArchitectureTheme {
+
+            }
+        }
+    }
+}
